92ecd7f7a1422b1b6bbf389007c324e760774550
Nathaniel Smith <qwrx123@gmail.com>
2025-11-10 19:08:49 -0800

    Added basic register manipulation functions for c

---
+++ arch/arm64/memorymanager/socfunctions.h
@@ -0,0 +1,39 @@
+#ifndef SOCFUNCTIONS_H
+#define SOCFUNCTIONS_H
+
+#include "kernel/types.h"
+
+static inline void write_mair_el1(uint64_t mair)
+{
+	__asm__ volatile("msr MAIR_EL1, %0" ::"r"(mair) : "memory");
+}
+static inline uint64_t read_mair_el1()
+{
+	uint64_t mair;
+	__asm__ volatile("mrs %0, MAIR_EL1" : "=r"(mair));
+	return mair;
+}
+
+static inline void write_tcr_el1(uint64_t tcr)
+{
+	__asm__ volatile("msr TCR_EL1, %0" ::"r"(tcr) : "memory");
+}
+static inline uint64_t read_tcr_el1()
+{
+	uint64_t tcr;
+	__asm__ volatile("mrs %0, TCR_EL1" : "=r"(tcr));
+	return tcr;
+}
+
+static inline void write_ttbr0_el1(uint64_t ttbr)
+{
+	__asm__ volatile("msr TTBR0_EL1, %0" ::"r"(ttbr) : "memory");
+}
+static inline uint64_t read_ttbr0_el1()
+{
+	uint64_t ttbr;
+	__asm__ volatile("mrs %0, TTBR0_EL1" : "=r"(ttbr));
+	return ttbr;
+}
+
+#endif

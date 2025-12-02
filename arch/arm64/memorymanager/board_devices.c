c4def7e6ed123da5e1995189c794c1cfcceb3c75
Nathaniel Smith <qwrx123@gmail.com>
2025-12-02 11:20:03 -0800

    Seperated devices into own class

---
+++ arch/arm64/memorymanager/board_devices.c
@@ -0,0 +1,16 @@
+#include "board_devices.h"
+#include "mmu.h"
+
+const device_config_t early_device_map[] = {
+	{ .name = "uart0",
+	  .base_addr = UART0_BASE_PHYS,
+	  .size = UART0_SIZE,
+	  .mem_type = MT_DEVICE_nGnRnE,
+	  .access_perm = AP_RW_EL1,
+	  .shareability = SH_OUTER,
+	  .pxn = PTE_PXN,
+	  .uxn = PTE_UXN },
+};
+
+const size_t early_device_map_count =
+	sizeof(early_device_map) / sizeof(early_device_map[0]);

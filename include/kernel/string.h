04e0c3270a419b0a33c43e051f291eebfd56c0b6
Nathaniel Smith <qwrx123@gmail.com>
2025-12-10 13:06:32 -0800

    Lots of comments

--- include/kernel/string.h
+++ include/kernel/string.h
@@ -3,6 +3,14 @@
 
 #include "kernel/types.h"
 
-extern void * memset(void *,int,uint64_t);
+/**
+ * @brief Sets section of memory to number
+ * 
+ * @param dest Address of memory to set
+ * @param ch Value to set memory to
+ * @param count Number of bytes to set
+ * @return void* Address of block set
+ */
+extern void *memset(void *dest, int ch, uint64_t count);
 
 #endif

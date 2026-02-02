0acbb7586bf1b7be35996f3428fa9bca4607f2ce
Sharderai <shadowstardiamond@yahoo.com>
2026-02-02 00:16:35 -0800

    some outlining for my current scheduler idea

--- arch/arm64/scheduler/scheduler.h
+++ arch/arm64/scheduler/scheduler.h
@@ -7,4 +7,17 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
+#include "proc.h"
+#include "proc_list.h"
+
+//ready queue
+//sleep queue
+
+//add new process to queue
+void procReady();
+//switch process
+void procSwitch();
+//put sleeping processes on sleep queue
+void procSleep();
+
 #endif

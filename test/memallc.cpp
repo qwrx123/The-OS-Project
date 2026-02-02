bc2db50c057a731e28be56d9ef0ed59e32945343
JordenWB <jorden.wilseybacso@bellevuecollege.edu>
2026-02-02 11:35:59 -0800

    finished adding all of the memalloc tests

--- test/memallc.cpp
+++ test/memallc.cpp
@@ -33,22 +33,38 @@ TEST(memallc_test, set_heap_e_out_of_range)
 
 TEST(memallc_test, memallc_under_min_bytes)
 {
+    uint64_t min_bytes = 0x00000000000001F0;
+    memallc(0x000000000000000A0);
+    EXPECT_EQ(get_heap_e(), get_heap_s() + min_bytes);
 }
 
 TEST(memallc_test, memallc_min_bytes)
 {
+    uint64_t min_bytes = 0x00000000000001F0;
+    memallc(min_bytes);
+     EXPECT_EQ(get_heap_e(), get_heap_s() + min_bytes);
 }
 
 TEST(memallc_test, memallc_in_range)
 {
+    uint64_t bytes = 0x1000;
+    memallc(bytes);
+    EXPECT_EQ(get_heap_e(), get_heap_s() + bytes);
 }
 
 TEST(memallc_test, memallc_max)
 {
+    uint64_t range = 0x400000;
+    uint64_t max_bytes = range - get_heap_s();
+    memallc(max_bytes);
+    EXPECT_EQ(get_heap_e(), get_heap_s() + max_bytes);
 }
 
 TEST(memallc_test, memallc_over_max)
 {
+    uint64_t range = 0x400000;
+    memallc(range + range);
+    EXPECT_EQ(get_heap_e(), get_heap_s() + range);
 }
 
 TEST(memallc_test, free_memallc)

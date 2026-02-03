6ae34786b01c3935569f123a50363c26609fb601
JordenWB <jorden.wilseybacso@bellevuecollege.edu>
2026-02-03 08:45:24 -0800

    initializing and setting and getting functions are done

--- driver/memallc.c
+++ driver/memallc.c
@@ -11,6 +11,29 @@ uintptr_t heap_e;
 uint64_t min_bytes = 0x00000000000001F0;
 uint64_t range = 0x200000;
 
+void init_memallc(uintptr_t s, uintptr_t e, uint64_t r)
+{
+    heap_s = s;
+    range = r;
+    set_heap_e(e);
+}
+
+void set_heap_bounds(uintptr_t s, uintptr_t e)
+{
+    heap_s = s;
+    set_heap_e(e);
+}
+
+void set_range(uint64_t r)
+{
+    range = r;
+}
+
+uint64_t get_range()
+{
+    return range;
+}
+
 void set_heap_e(uintptr_t e)
 {
     if (e > (heap_s + range))

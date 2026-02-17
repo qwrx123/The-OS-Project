5eafe677043d2099a4f468a2ab33c2cf635ca1fb
Sharderai <shadowstardiamond@yahoo.com>
2026-02-16 19:31:33 -0800

    added register context struct in process

--- arch/arm64/scheduler/proc.h
+++ arch/arm64/scheduler/proc.h
@@ -7,6 +7,13 @@
 #ifndef PROC_H
 #define PROC_H
 
+struct proc
+{
+    state proc_state;
+    context proc_context;
+};
+
+
 typedef enum
 {
     READY,
@@ -15,10 +22,26 @@ typedef enum
     ZOMBIE
 } state;
 
-struct proc
-{
-    state proc_state;
-    context proc_context;
+
+struct context{
+    uint64_t sp; //Stack pointer
+    uint64_t lr; //Link register
+    uint64_t pc; //Program counter
+    uint64_t apsr; //Application program status register
+    //General registers
+    uint64_t r0;
+    uint64_t r1;
+    uint64_t r2;
+    uint64_t r3;
+    uint64_t r4;
+    uint64_t r5;
+    uint64_t r6;
+    uint64_t r7;
+    uint64_t r8;
+    uint64_t r9;
+    uint64_t r10;
+    uint64_t r11;
+    uint64_t r12;
 };
 
 #endif

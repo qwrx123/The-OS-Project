a252c2026a708cbe29dc2ce97012cb9cf42caeb1
Sharderai <shadowstardiamond@yahoo.com>
2026-02-17 09:24:42 -0800

    changed register list to aarch64 register list

--- arch/arm64/scheduler/proc.h
+++ arch/arm64/scheduler/proc.h
@@ -24,24 +24,43 @@ typedef enum
 
 
 struct context{
-    uint64_t sp; //Stack pointer
-    uint64_t lr; //Link register
-    uint64_t pc; //Program counter
-    uint64_t apsr; //Application program status register
-    //General registers
-    uint64_t r0;
-    uint64_t r1;
-    uint64_t r2;
-    uint64_t r3;
-    uint64_t r4;
-    uint64_t r5;
-    uint64_t r6;
-    uint64_t r7;
-    uint64_t r8;
-    uint64_t r9;
-    uint64_t r10;
-    uint64_t r11;
-    uint64_t r12;
+    uint64_t SP; //Stack pointer
+    uint64_t PSTATE; //Processor state
+    uint64_t x30; //Link register
+    uint64_t x29; //Frame pointer
+    uint64_t x18; //Global pointer
+
+    uint64_t x0;
+
+    uint64_t x1;
+    uint64_t x2;
+    uint64_t x3;
+    uint64_t x4;
+    uint64_t x5;
+    uint64_t x6;
+    uint64_t x7;
+
+    uint64_t x8;
+    uint64_t x9;
+    uint64_t x10;
+    uint64_t x11;
+    uint64_t x12;
+    uint64_t x13;
+    uint64_t x14;
+    uint64_t x15;
+    uint64_t x16;
+    uint64_t x17;
+    
+    uint64_t x19;
+    uint64_t x20;
+    uint64_t x21;
+    uint64_t x22;
+    uint64_t x23;
+    uint64_t x24;
+    uint64_t x25;
+    uint64_t x26;
+    uint64_t x27;
+    uint64_t x28;
 };
 
 #endif

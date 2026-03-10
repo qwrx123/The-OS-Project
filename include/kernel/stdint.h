f9d6aa295c69cc8baca70a78c187a5597590bed3
Sharderai <shadowstardiamond@yahoo.com>
2026-03-09 19:25:01 -0700

    copied the multiple declaration fix from memallc branch

---
+++ include/kernel/stdint.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 Nathaniel Smith
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _STDINT_H
+#define _STDINT_H
+
+typedef unsigned long long uint64_t;
+typedef unsigned char uint8_t;
+typedef uint64_t uintptr_t;
+
+#endif
--- include/kernel/types.h
+++ include/kernel/types.h
@@ -4,12 +4,11 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include "kernel/stdint.h"
+
 #ifndef TYPES_H
 #define TYPES_H
 
-typedef unsigned long long uint64_t;
-typedef unsigned char uint8_t;
-typedef uint64_t uintptr_t;
 typedef uint64_t phys_addr_t;
 
 #endif

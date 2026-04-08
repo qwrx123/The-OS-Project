a61e8f0430d449e5000184d2a3ce655094b139a7
Nathaniel Smith <qwrx123@gmail.com>
2026-04-08 14:20:16 -0700

    Added formatting check

--- cmake/helpfulFunctions.cmake
+++ cmake/helpfulFunctions.cmake
@@ -17,24 +17,34 @@ function(registerFilesToFormat)
 
 	foreach(file IN LISTS TARGET_FILES)
 
-		set(Code "Format${file}")
-		set(Target "Target${file}")
+        set(Code "Format${file}")
+        set(Target "Target${file}")
+        set(Check "CheckFormat${file}")
 
-		set(fullTargetPath ${CMAKE_CURRENT_LIST_DIR}/${file})
-		file(RELATIVE_PATH filePath ${CMAKE_SOURCE_DIR} ${fullTargetPath})
-		message(STATUS "Targeting ${filePath} for formatting")
-		
-		
-		add_custom_target(${Code} ALL
-			COMMAND ${CLANG_FORMAT} -i -style=file ${fullTargetPath}
-			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-		)
+        set(fullTargetPath ${CMAKE_CURRENT_LIST_DIR}/${file})
+        file(RELATIVE_PATH filePath ${CMAKE_SOURCE_DIR} ${fullTargetPath})
+        message(STATUS "Targeting ${filePath} for formatting")
+
+        add_custom_target(${Code} ALL
+            COMMAND ${CLANG_FORMAT} -i -style=file ${fullTargetPath}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        )
+
+        # Non-mutating check target for CI
+        add_custom_target(${Check}
+            COMMAND ${CLANG_FORMAT} --dry-run -Werror -style=file ${fullTargetPath}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        )
 
 		add_custom_target(${Target} ALL
 			SOURCES ${file}
 		)
 
 		add_dependencies(${Target} ${Code})
+
+        if(TARGET format-check)
+            add_dependencies(format-check ${Check})
+        endif()
 
 	endforeach()
 endfunction()

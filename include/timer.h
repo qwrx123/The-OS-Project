f86c538ca113e841e26b480d71920a9cf44ab616
JordenWB <jorden.wilseybacso@bellevuecollege.edu>
2026-04-21 09:34:49 -0700

    filled out timer.h

---
+++ include/timer.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) [2026] Jorden Wilsey Bacso
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "kernel/types.h"
+
+/*
+uint64_t count = CNTPCT_EL0;
+uint64_t freq = CNTFRQ_EL0;
+uint64_t interupt_timer = CNTPS_CVAL_EL1;
+*/
+
+uint64_t freq;
+uint64_t time;
+
+// typedef struct 
+
+/**
+ * @brief Initializes the timer.
+ */
+void init_timer();
+
+/**
+ * @brief Enables the timer.
+ */
+void enable_timer();
+
+/**
+ * @brief Handles the timer interrupt.
+ */
+void timer_interrupt();
+
+/**
+ * @brief Gets the current timer value.\
+ * @return The current timer value.
+ */
+uint64_t get_time();
+
+/**
+ * @brief Gets the timer frequency.
+ * @return The timer frequency.
+ */
+uint64_t get_timer_freq();
+
+#endif

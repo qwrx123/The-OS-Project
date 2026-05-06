b06e28f23befde3825f00f3782fb558d1061a580
JordenWB <jorden.wilseybacso@bellevuecollege.edu>
2026-05-06 13:23:14 -0700

    displays how many secconds every seccond when running

--- driver/timer.c
+++ driver/timer.c
@@ -115,8 +115,38 @@ uint64_t get_timer_sec()
 void timer_to_string()
 {
 #ifdef QEMU
-	uart_puts(
-		"timer: kernel has been running for [unknown] seconds (QEMU)\r\n");
+	static char str_sec[20];
+	int len = 20;
+	uint64_t sec = get_timer_sec();
+	int i = len - 1;
+	str_sec[i] = '\0';
+
+	if (sec == 0)
+	{
+		if (i > 0)
+		{
+			str_sec[--i] = '0';
+		}
+	}
+	else
+	{
+		while (sec > 0 && i > 0)
+		{
+			str_sec[--i] = (char)('0' + (sec % 10));
+			sec /= 10;
+		}
+	}
+
+	int j = 0;
+	while (str_sec[i] != '\0')
+	{
+		str_sec[j++] = str_sec[i++];
+	}
+	str_sec[j] = '\0';
+
+	uart_puts("timer: kernel has been running for ");
+	uart_puts(str_sec);
+	uart_puts(" seconds(QEMU)\r\n");
 	return;
 #else
 	uart_puts("timer: kernel has been running for ");

8e2352c6ead03a5e69405ce4fbc43d7690741b57
Nathaniel Smith <qwrx123@gmail.com>
2026-05-21 09:04:54 -0700

    irq has replaced function with itoa

--- arch/arm64/kernel/irq.c
+++ arch/arm64/kernel/irq.c
@@ -7,32 +7,15 @@
 #include <kernel/stdint.h>
 #include "gic.h"
 #include "uart.h"
+#include "stdlib.h"
 
 extern void uart_puts(const char *s);
 
 void el1_irq_handler(/*struct pt_regs *regs*/)
 {
 	uint32_t irq = gic_get_active_irq();
-	uint32_t translate_irq = irq;
 	char readable_irq[13] = { 0 };
-	//this is definitely a function
-	while (translate_irq != 0)
-	{
-		uint8_t single_digit = (translate_irq % 10) + '0';
-		translate_irq /= 10;
-		int i;
-		for (i = 0; i < 13 && readable_irq[i] != '\0'; i++)
-		{
-			char temp_char = readable_irq[i];
-			readable_irq[i] = single_digit;
-			single_digit = temp_char;
-		}
-		if (i < 13)
-		{
-			readable_irq[i] = single_digit;
-		}
-		readable_irq[12] = '\0';
-	}
+	itoa(irq, readable_irq, 10);
 	uart_puts("el1_irq number ");
 	uart_puts(readable_irq);
 	uart_puts("\r\n");
